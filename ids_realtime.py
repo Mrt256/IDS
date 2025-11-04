cd33140bc3e8d8b8f023ec222e1041c2389e547d
Mrt256 <mathheste@gmail.com>
2025-11-04 10:48:07 -0300

    update ids_realtime

--- ids_realtime.py
+++ ids_realtime.py
@@ -1,2 +1,5 @@
-import xgboost
-print(xgboost.__version__)
+import pyshark
+
+capture = pyshark.LiveCapture(interface='Wi-Fi') #Ethernet
+for packet in capture.sniff_continuously(packet_count=5):
+    print(packet.highest_layer, packet.length, packet.sniff_time)
